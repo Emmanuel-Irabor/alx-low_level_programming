ba75afc599b76de66b9379f7cacedf729e3731e8
Emmanuel-Irabor <emmanuel4sure93@gmail.com>
2023-02-16 14:37:52 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: Prints a string to the standard output
- * Return: 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n";
-			return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }

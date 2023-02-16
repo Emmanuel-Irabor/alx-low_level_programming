9bd01504adc638bb222ec00b4d192b713be24354
Emmanuel-Irabor <emmanuel4sure93@gmail.com>
2023-02-16 14:43:14 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Description: Prints a string to the standard error
- * Return: 1 (Error)
+ * main - Entry
+ * Return: 1
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	const size_t len = sizeof(msg) - 1;
-	
-	ssize_t ret = write(STDERR_FILENO, msg, len);
-	return (ret == len) ? 1 : -1;
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+	return (1);
 }
-

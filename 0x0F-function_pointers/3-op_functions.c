f3e3784a8dd14702015492cc8fb3a7a4042189a4
Emmanuel-Irabor <emmanuel4sure93@gmail.com>
2023-03-22 13:53:50 -0700

    Completed

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}

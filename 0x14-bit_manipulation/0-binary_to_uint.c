4aaabf89aed8a69b2a8708443df6325c11f6c643
Emmanuel-Irabor <emmanuel4sure93@gmail.com>
2023-04-08 00:19:33 -0700

    Fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    if (!b) {
-        return 0;
-    }
+	int d;
+	unsigned int decimal_val = 0;
 
-    unsigned int dec_val = 0;
+	if (!b)
+		return (0);
 
-    for (int i = 0; b[i]; i++) {
-        if (b[i] != '0' && b[i] != '1') {
-            return 0;
-        }
+	for (d = 0; b[d]; d++)
+	{
+		if (b[d] < '0' || b[d] > '1')
+			return (0);
+		decimal_val = 2 * decimal_val + (b[d] - '0');
+	}
 
-        dec_val = 2 * dec_val + (b[i] - '0');
-    }
-
-    return dec_val;
+	return (decimal_val);
 }
-
